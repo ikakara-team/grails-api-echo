25106df51edadeb17c3a56256eb7bdc0fbfba02c
ikakara <allen.arakaki@gmail.com>
2015-02-12 22:36:31 -0800

    fix body content

--- grails-app/controllers/ikakara/web/EchoController.groovy
+++ grails-app/controllers/ikakara/web/EchoController.groovy
@@ -19,6 +19,7 @@ import org.apache.commons.io.IOUtils
 import grails.converters.*
 
 class EchoController {
+  private static final HashSet PARAMS_FILTER = ['action', 'controller', 'path']
 
   def index() {
     println "echo ${request.contextPath} servlet: ${request.servletPath} path: ${request.pathInfo} params: ${params}"
@@ -31,9 +32,41 @@ class EchoController {
       map.put(name, value);
     }
 
+    // parseRequest: false is broken - we'll have to get the POST data in params
+    try {
+      // this is broken
+      def body = request.getReader().text // read the content body
+      log.info "parseRequest: false - WORKING WORKING WORKING WORKING WORKING!"
+    } catch(e) {
+      // this will be
+      log.error "parseRequest: false - " + e.getMessage()
+    }
+
+    // this doesn't behave the same as request.getReader().text
     def enc = request.characterEncoding ?: "UTF-8"
-    def is = request.inputStream
+    def is = request.getInputStream()
     def body = IOUtils.toString(is, enc)
+    if(!body) {
+      HashSet setQuery = []
+      def queryParams = request.queryString?.split('&')
+      queryParams?.each {
+        def param = it.split('=')
+        if(param?.length > 0) {
+          setQuery.add(param[0])
+        }
+      }
+
+      body = [:]
+      params.each {
+        if(PARAMS_FILTER.contains(it.key)) {
+          // ignore
+        } else if(setQuery.contains(it.key)) {
+          // ignore the queryString params
+        } else {
+          body.put(it.key, it.value)
+        }
+      }
+    }
 
     render(contentType: "application/json", encoding: enc) {
       [

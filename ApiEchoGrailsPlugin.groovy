9b85ed43f66f9a8b7add839c4f4c47159548351e
Burt Beckwith <burt@burtbeckwith.com>
2015-02-13 21:21:51 -0500

    cleanup

--- ApiEchoGrailsPlugin.groovy
+++ ApiEchoGrailsPlugin.groovy
@@ -1,70 +1,15 @@
 class ApiEchoGrailsPlugin {
-  // the plugin version
   def version = "0.1"
-  // the version or versions of Grails the plugin is designed for
   def grailsVersion = "2.0 > *"
-  // resources that are excluded from plugin packaging
-  def pluginExcludes = [
-        "grails-app/views/error.gsp"
-  ]
-
-  // TODO Fill in these fields
-  def title = "API Echo Plugin" // Headline display name of the plugin
+  def title = "API Echo Plugin"
   def author = "Allen Arakaki"
   def authorEmail = ""
   def description = '''
 Used to help debug 3rd party API requests, by echoing the contents of the request back to the user to insure that
 requests are properly formed.
 '''
-
-  // URL to the plugin's documentation
   def documentation = "http://grails.org/plugin/api-echo"
-
-  // Extra (optional) plugin metadata
-
-  // License: one of 'APACHE', 'GPL2', 'GPL3'
   def license = "APACHE"
-
-  // Details of company behind the plugin (if there is one)
-  //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-  // Any additional developers beyond the author specified above.
-  //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-  // Location of the plugin's issue tracker.
-  //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-  // Online location of the plugin's browseable source code.
-  //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-  def doWithWebDescriptor = { xml ->
-    // TODO Implement additions to web.xml (optional), this event occurs before
-  }
-
-  def doWithSpring = {
-    // TODO Implement runtime spring config (optional)
-  }
-
-  def doWithDynamicMethods = { ctx ->
-    // TODO Implement registering dynamic methods to classes (optional)
-  }
-
-  def doWithApplicationContext = { ctx ->
-    // TODO Implement post initialization spring config (optional)
-  }
-
-  def onChange = { event ->
-    // TODO Implement code that is executed when any artefact that this plugin is
-    // watching is modified and reloaded. The event contains: event.source,
-    // event.application, event.manager, event.ctx, and event.plugin.
-  }
-
-  def onConfigChange = { event ->
-    // TODO Implement code that is executed when the project configuration changes.
-    // The event is the same as for 'onChange'.
-  }
-
-  def onShutdown = { event ->
-    // TODO Implement code that is executed when the application shuts down (optional)
-  }
+  def issueManagement = [url: 'https://github.com/ikakara-team/grails-api-echo/issues']
+  def scm = [url: 'https://github.com/ikakara-team/grails-api-echo']
 }

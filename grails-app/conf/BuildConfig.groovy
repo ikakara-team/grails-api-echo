aacb8fd99a92444779d3d29f189018f6ed44af0a
ikakara <allen.arakaki@gmail.com>
2015-03-26 11:18:52 -0700

    fix https://jira.grails.org/browse/GPRELEASE-64

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,7 +20,7 @@ grails.project.dependency.resolution = {
       export = false
     }
 
-    build(":release:3.1.0", ":rest-client-builder:2.1.0") {
+    build(":release:3.1.1", ":rest-client-builder:2.1.1") {
       export = false
     }
   }
